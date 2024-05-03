b1c2ab3a1a0e38320559f3c19ef94aed58e5af73
dirtyboy6 <160387841+dirtyboy6@users.noreply.github.com>
2024-05-03 19:16:45 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+'use strict';
+
+{
+  const text = document.getElementById('text');
+  const save = document.getElementById('save');
+  const clear = document.getElementById('clear');
+  const message = document.getElementById('message');
+
+  if (localStorage.getItem('memo') === null) {
+    text.value = '';
+  } else {
+    text.value = localStorage.getItem('memo');
+  }
+
+
+  save.addEventListener('click', () => {
+    message.classList.add('appear');
+    setTimeout(() => {
+      message.classList.remove('appear');
+    }, 1000);
+    localStorage.setItem('memo', text.value);
+  });
+
+  clear.addEventListener('click', () => {
+    if (confirm('本当に消去しますか？') === true) {
+      text.value = '';
+      localStorage.removeItem('memo');
+    }
+  });
+}
